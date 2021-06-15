52fb5da69a2aadc119d2fc35a24846bbd6741664
sanjitdp <sanjitd@ucla.edu>
2021-06-14 17:49:58 -0700

    created transition matrix from words in document

--- src/markov-chain.py
+++ src/markov-chain.py
@@ -1,8 +1,13 @@
 from random import choices
 
 
-def read_file(filepath):
-    return open(filepath, "r", encoding="utf8")
+class ProbabilityList:
+    def __init__(self, word):
+        self.next_word_probability[word] = 1
+        self.total = 1
+
+    next_word_probability = {}
+    total = 0
 
 
 def get_distinct_words(file):
@@ -16,13 +21,31 @@ def get_distinct_words(file):
     return distinct_words
 
 
-def create_transition_matrix():
+def read_file(filepath):
+    return open(filepath, "r", encoding="utf8").read()
+
+
+def get_transition_matrix(file):
     transition_matrix = {}
+
+    words = file.split()
+    for index, word in enumerate(words):
+        if index == len(words) - 1:
+            break
+        if word not in transition_matrix:
+            transition_matrix[word] = ProbabilityList(words[index + 1])
+        else:
+            if words[index + 1] not in transition_matrix[word].next_word_probability:
+                transition_matrix[word].next_word_probability[words[index + 1]] = 1
+            else:
+                transition_matrix[word].next_word_probability[words[index + 1]] += 1
+            transition_matrix[word].total += 1
+
     return transition_matrix
 
 
 def select_word(transition_matrix, starting_sequence):
-    prob_dist = transition_matrix.get(starting_sequence).getTransitionMatrix()
+    prob_dist = transition_matrix[starting_sequence].next_word_probability
     words = []
     vals = []
     for k, v in prob_dist:
